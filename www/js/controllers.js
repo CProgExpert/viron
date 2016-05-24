4d9b11a7a225f0fd8133eab823224aa922e15da6
Rashawn Clarke <finish101@hotmail.com>
2016-05-23 23:49:02 -0400

    Project

---
+++ www/js/controllers.js
@@ -0,0 +1,59 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+})
+
+.controller('CameraCtrl', function ($scope, $stateParams, $cordovaCamera, $cordovaGeolocation){
+  
+  $scope.takePhoto = function () {
+    var options = {
+      destinationType: 0,
+      sourceType: 1,
+      encodingType: 0,
+    };
+
+    $cordovaCamera.getPicture(options).then(function(imageData) {
+      var img = document.getElementById('myImage');
+      img.src = "data:image/jpeg;base64," + imageData;
+    }, function(err) {
+      // error
+      console.log(err)
+    });
+  };
+  
+  $scope.createPost = function(post) {
+    var options = {
+      timeout: 5000,
+      enableHighAccuracy: false
+    }
+    $cordovaGeolocation.getCurrentPosition(options)
+      .then(function(position){
+        post.latitude = position.coords.latitude;
+        post.longitude = position.coords.longitude;
+      });
+  };
+});
